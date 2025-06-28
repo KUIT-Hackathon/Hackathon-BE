c4eba3133f319b64046d83ac4f5865ba35d4cd9c
김태성 <ts28@gimtaeseong-ui-MacBookPro.local>
2025-06-29 07:15:18 +0900

    [FIX] notification 없으면 빈 리스트 내리도록 수정

--- src/main/java/com/example/hackathonbe/notification/service/GetNotificationService.java
+++ src/main/java/com/example/hackathonbe/notification/service/GetNotificationService.java
@@ -1,11 +1,8 @@
 package com.example.hackathonbe.notification.service;
 
-import com.example.hackathonbe.global.config.BusinessException;
-import com.example.hackathonbe.global.config.ErrorCode;
 import com.example.hackathonbe.notification.dto.GetNotificationResponseDto;
 import com.example.hackathonbe.notification.repository.AlarmRepository;
 import com.example.hackathonbe.notification.repository.NotificationRepository;
-import com.example.hackathonbe.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,13 +13,9 @@ import java.util.List;
 public class GetNotificationService {
     private final NotificationRepository notificationRepository;
     private final AlarmRepository alarmRepository;
-    private final UserRepository userRepository;
 
     public GetNotificationResponseDto get(Long userId) {
         List<Long> notificationIdsList = notificationRepository.findAllByUserId(userId).stream().map(it -> it.getAlarmId()).toList();
-        if (notificationIdsList.isEmpty()) { // 유저에게 보여줄 알림이 없으면 에러 내림
-            throw new BusinessException(ErrorCode.NO_NOTIFICATIONS_FOUND);
-        }
 
         return new GetNotificationResponseDto(alarmRepository.findAllByIdIn(notificationIdsList));
     }
