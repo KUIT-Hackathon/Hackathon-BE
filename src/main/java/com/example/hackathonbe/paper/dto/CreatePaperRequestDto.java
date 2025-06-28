fe77b3cbaa6451cb272694df539bdf4ceea19683
김태성 <ts28@gimtaeseong-ui-MacBookPro.local>
2025-06-29 00:33:36 +0900

    [REFACTOR] LocalDateTime -> LocalDatte 로 수정

--- src/main/java/com/example/hackathonbe/paper/dto/CreatePaperRequestDto.java
+++ src/main/java/com/example/hackathonbe/paper/dto/CreatePaperRequestDto.java
@@ -5,13 +5,13 @@ import com.example.hackathonbe.paper.entity.enums.VisibilityScope;
 import jakarta.annotation.Nullable;
 import lombok.Getter;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 @Getter
 public class CreatePaperRequestDto {
     @Nullable
     private Long owner;
-    private LocalDateTime publishDate;
+    private LocalDate publishDate;
     private String title;
     private VisibilityScope visibilityScope;
     private Category category;
